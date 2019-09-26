2259c826821106c2f41f9b4d083628deaa34501f
LAB316 Student02 <sabakevlishvili321@gmail.com>
2019-09-26 19:46:35 +0400

    Test

---
+++ tornike/week_2/week2_1/script.js
@@ -0,0 +1,18 @@
+function testJsConsle(){
+    console.log("Hello JS, I am console!!!");
+}
+
+function testJsAlert(){
+    alert("Hello JS, I am alert!!!");
+}
+
+function sumOfTwoNumbers(a, b){
+    console.log(a+b);
+}
+
+
+function textInputDiv(text){
+    x = document.getElementById("result");
+    x.innerText = text;
+}
+
